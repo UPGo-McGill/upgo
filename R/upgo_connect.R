c1a0e9ae65fb5204bd815560709ea8faef295553
dwachsmuth <david.wachsmuth@mcgill.ca>
2019-09-19 16:31:12 -0400

    upgo_connect working, but with no documentation

---
+++ R/upgo_connect.R
@@ -0,0 +1,10 @@
+upgo_connect <- function(property = TRUE, daily = TRUE, reviews = FALSE) {
+  con <<- RPostgres::dbConnect(
+    RPostgres::Postgres(),
+    host = "025wpgs.campus.mcgill.ca",
+    dbname = "airdna")
+
+  if (property) property_all <<- dplyr::tbl(con, "property")
+  if (daily) daily_all <<- dplyr::tbl(con, "daily")
+  if (reviews) reviews_all <<- dplyr::tbl(con, "reviews")
+}
