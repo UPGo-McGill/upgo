79aa10e94d5330a83f3c69dd2b43df7b04a4cec1
dwachsmuth <david.wachsmuth@mcgill.ca>
2019-11-07 21:26:14 -0500

    Overhauled upgo_location_scrape to use either Docker or a headless browser, and with better waiting for the connection

--- R/upgo_location_scrape.R
+++ R/upgo_location_scrape.R
@@ -3,17 +3,15 @@
 #' \code{upgo_location_scrape} scrapes location (city, region and country) from
 #' Airbnb listings.
 #'
-#' TKTK
+#' A function for scraping location information from Airbnb listings through
+#' RSelenium, using either a headless Chrome browser or a Docker container.
 #'
 #' @param property An input table with a field named \code{property_ID} which
 #' will be used to generate URLs for scraping.
 #' @param port The port to use to connect to Docker container. If NULL (the
 #' default), an interactive Chrome window will be opened instead.
-#' @param delay A numeric scalar. The number of seconds the function should
-#' delay in between listings to prevent connection errors or Airbnb shutting
-#' down access.
-#' @param geography An existing output table which the function should add to.
-#' By default the function creates a new table instead.
+#' @param docker A logical scalar. Should the scraping be done via a Docker
+#' container, or by launching a headless Chrome window (default)?
 #' @import RSelenium
 #' @importFrom dplyr last
 #' @importFrom tibble tibble
@@ -23,25 +21,32 @@
 #' @export
 
 
-upgo_location_scrape <- function(property, port = NULL, delay = 10,
-                                 geography = NULL) {
+upgo_location_scrape <- function(property, port = 4444L, docker = FALSE) {
 
-  ### Initialize table if geography is NULL
+  ### Initialize table
 
-  if (missing(geography)) {
-    geography <- tibble(property_ID = character(0),
-                        city = character(0),
-                        region = character(0),
-                        country = character(0),
-                        raw = character(0))
-  }
+  geography <- tibble(
+    property_ID = character(0),
+    city = character(0),
+    region = character(0),
+    country = character(0),
+    raw = character(0)
+  )
 
 
   ### Start browser
-  if (missing(port)) {
-    rD <- rsDriver(browser = "chrome", chromever = "77.0.3865.40")
-    remDr <- rD$client
 
+  if (!docker) {
+    eCaps <- list(chromeOptions = list(
+      args = c("--headless", "--disable-gpu", "--window-size=1280,800"),
+      w3c = FALSE
+    ))
+
+    rD <- rsDriver(
+      port = as.integer(port), browser = "chrome",
+      chromever = "78.0.3904.70", extraCapabilities = eCaps
+    )
+    remDr <- rD$client
   } else {
     remDr <- remoteDriver(browserName = "chrome", port = as.integer(port))
     remDr$open()
@@ -53,21 +58,13 @@ upgo_location_scrape <- function(property, port = NULL, delay = 10,
   ### For loop to retrieve geographies
 
   for (i in seq_along(PIDs)) {
-
-    ## Determine whether to skip the row
-
-    # Skip row if it has already been scraped
-    if (!is.na(geography[i, 5])) {
-      message("Listing ", i, " was previously scraped.")
-      next
-      }
-
     geography[i, 1] <- PIDs[i]
 
     geography[i, 2] <- geography[i, 3] <- geography[i, 4] <- geography[i, 5] <-
       NA_character_
 
-    # Skip row if it is HomeAway
+    ## Skip row if it is HomeAway
+
     if (str_detect(PIDs[i], "ha-")) {
       geography[i, 5] <- "HOMEAWAY"
       message("Listing ", i, " is a HomeAway listing.")
@@ -77,69 +74,83 @@ upgo_location_scrape <- function(property, port = NULL, delay = 10,
 
     ## Initialize connection
 
-    # Extract URL and navigate to page, then wait
+    # Extract URL and navigate to page
     PID <- str_extract(PIDs[i], "(?<=ab-).*")
     remDr$navigate(paste0("https://www.airbnb.ca/rooms/", PID))
-    Sys.sleep(delay)
 
-    # If listing redirects to homepage, note this and move on
-    if (remDr$getTitle()[[1]] == "Anywhere · Stays · Airbnb") {
-      geography[i, 5] <- "NO LISTING"
-      message("Listing ", i, " no longer exists.")
-      next
-    }
+    # Set waiting time
+    remDr$setImplicitWaitTimeout(5000)
 
 
     ## Get elements
 
-    # Check to see if the listing is Airbnb plus
-    if (str_detect(remDr$getCurrentUrl(), "plus")) {
-      location <- remDr$findElements("class", "_ylytgbo")
-
-    # If not, proceed normally
-    } else {
-      location <- remDr$findElements("class", "_czm8crp")
-    }
+    location <- remDr$findElements("class", "_czm8crp")
 
     elements <-
-      map_chr(location, ~{
+      map_chr(location, ~ {
         .x$getElementAttribute("outerHTML")[[1]]
       })
 
-    tries <- 3
-
     # Try to refetch data three times before giving up
+    tries <- 5
+
     while (length(elements) == 0 & tries > 0) {
 
-      Sys.sleep(2)
+      # If listing redirects to homepage, break out of while loop
+      if (remDr$getTitle()[[1]] == "Anywhere · Stays · Airbnb") {
+        break
+      }
+
+      Sys.sleep(1)
 
       location <- remDr$findElements("class", "_czm8crp")
 
       elements <-
-        map_chr(location, ~{
+        map_chr(location, ~ {
           .x$getElementAttribute("outerHTML")[[1]]
         })
 
       tries <- tries - 1
     }
 
+
+    ## Advance for loop if listing is defunct
+
+    if (remDr$getTitle()[[1]] == "Anywhere · Stays · Airbnb") {
+      geography[i, 5] <- "NO LISTING"
+      message("Listing ", i, " no longer exists.")
+      next
+    }
+
+    ## Abort if elements is still empty
+
     if (length(elements) == 0) {
-      message("Connection problem; scraping aborted.")
+      message("Connection problem 1; scraping aborted.")
       remDr$close()
-      if (missing(port)) rD$server$stop()
+      if (!docker) rD$server$stop()
       return(geography)
     }
 
-    # Extract Airbnb plus locations
+
+    ## Check to see if the listing is Airbnb plus
+
     if (str_detect(remDr$getCurrentUrl(), "plus")) {
+      location <- remDr$findElements("class", "_ylytgbo")
+
+      elements <-
+        map_chr(location, ~ {
+          .x$getElementAttribute("outerHTML")[[1]]
+        })
+
       elements_extracted <-
         elements %>%
-        str_extract('(?=>).+(?=<)') %>%
+        str_extract("(?=>).+(?=<)") %>%
         str_replace(">", "") %>%
         str_split(", ") %>%
         unlist()
 
-    # Conditional for all non-plus locations
+
+      ## Conditional for all non-plus locations
     } else {
 
       # Filter elements to ones containing key phrase
@@ -148,20 +159,32 @@ upgo_location_scrape <- function(property, port = NULL, delay = 10,
 
       # Aggressive exception catching to avoid the function terminating
       if (length(elements) == 0) {
-        message("Connection problem; scraping aborted.")
+        location <- remDr$findElements("class", "_czm8crp")
+
+        elements <-
+          map_chr(location, ~ {
+            .x$getElementAttribute("outerHTML")[[1]]
+          })
+
+        elements <-
+          elements[str_detect(elements, "’s place is located in")]
+      }
+
+      if (length(elements) == 0) {
+        message("Connection problem 2; scraping aborted.")
         remDr$close()
-        if (missing(port)) rD$server$stop()
+        if (!docker) rD$server$stop()
         return(geography)
       }
 
       # Try to filter out spurious uses of the "place is located in" phrase
-      if(length(elements) > 1 & sum(str_detect(elements, "span")) > 0) {
+      if (length(elements) > 1 & sum(str_detect(elements, "span")) > 0) {
         elements <-
           elements[str_detect(elements, "_abw475")]
       }
 
       # Logic for cases with standard place name separation
-      if(str_detect(elements, "span")) {
+      if (str_detect(elements, "span")) {
         elements_extracted <-
           elements %>%
           str_extract_all(('(?<="><span>).*?(?=</span>)')) %>%
@@ -172,13 +195,13 @@ upgo_location_scrape <- function(property, port = NULL, delay = 10,
       } else if (str_detect(elements, "\n, \n\n\n,")) {
         elements_extracted <-
           elements %>%
-          str_extract('(?<=, ).*(?=.<)')
+          str_extract("(?<=, ).*(?=.<)")
 
         # Logic for cases with one missing place in commas and exit early
       } else if (str_detect(elements, " \n\n,")) {
         elements_extracted <-
           elements %>%
-          str_extract('(?<=, ).*(?=.<)') %>%
+          str_extract("(?<=, ).*(?=.<)") %>%
           str_split(", ") %>%
           unlist()
 
@@ -197,13 +220,15 @@ upgo_location_scrape <- function(property, port = NULL, delay = 10,
         if (str_detect(elements, "Bonaire")) {
           elements_extracted <-
             elements %>%
-            str_extract('(?<=located in ).*(?=.<)') %>%
+            str_extract("(?<=located in ).*(?=.<)") %>%
             str_split(", ") %>%
             unlist()
 
           elements_extracted[length(elements_extracted) - 1] <-
             paste(elements_extracted[length(elements_extracted) - 1],
-                  elements_extracted[length(elements_extracted)], sep = ", ")
+              elements_extracted[length(elements_extracted)],
+              sep = ", "
+            )
 
           elements_extracted <- elements_extracted[-length(elements_extracted)]
 
@@ -211,7 +236,7 @@ upgo_location_scrape <- function(property, port = NULL, delay = 10,
         } else {
           elements_extracted <-
             elements %>%
-            str_extract('(?<=located in ).*(?=.<)') %>%
+            str_extract("(?<=located in ).*(?=.<)") %>%
             str_split(", ") %>%
             unlist()
         }
@@ -225,38 +250,38 @@ upgo_location_scrape <- function(property, port = NULL, delay = 10,
     if (length(elements_extracted) == 1) {
       geography[i, 4] <- elements_extracted
 
-    # With two elements, put them in city and country
+      # With two elements, put them in city and country
     } else if (length(elements_extracted) == 2) {
       geography[i, 2] <- elements_extracted[1]
       geography[i, 4] <- elements_extracted[2]
 
-    # With three elements, put them in city, region and country
+      # With three elements, put them in city, region and country
     } else if (length(elements_extracted) == 3) {
       geography[i, 2] <- elements_extracted[1]
       geography[i, 3] <- elements_extracted[2]
       geography[i, 4] <- elements_extracted[3]
 
-    # With more than three elements, only take country
+      # With more than three elements, only take country
     } else if (length(elements_extracted) > 3) {
       geography[i, 4] <- last(elements_extracted)
 
-    # Throw exception if none of the length conditions is satisfied
-    } else stop()
+      # Throw exception if none of the length conditions is satisfied
+    } else {
+      stop()
+    }
 
     geography[i, 5] <- elements
 
     .temp_scraping_table <<- geography
 
     message("Listing ", i, " successfully scraped.")
-
   }
 
   ### Close connection and return output
 
   remDr$close()
-  if (missing(port)) rD$server$stop()
+  if (!docker) rD$server$stop()
 
   rm(.temp_scraping_table, envir = .GlobalEnv)
   return(geography)
-
 }
