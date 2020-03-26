81eac58cba216f35abbbb563729a585f33efc583
David Wachsmuth <david.wachsmuth@mcgill.ca>
2020-03-25 23:42:06 -0400

    Improvement to location parsing in upgo_scrape_location

--- R/helper_scrape_location.R
+++ R/helper_scrape_location.R
@@ -119,6 +119,59 @@ helper_scrape_location <- function(PID) {
 
 helper_scrape_location_parse <- function(results) {
 
+  ### Country list for checking ################################################
+
+  country_list <-
+    c("Afghanistan", "\u00c5land Islands", "Albania", "Algeria",
+      "American Samoa", "Andorra", "Angola", "Anguilla", "Antigua and Barbuda",
+      "Argentina", "Armenia", "Aruba", "Australia", "Austria", "Azerbaijan",
+      "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium",
+      "Belize", "Benin", "Bermuda", "Bhutan", "Bolivia",
+      "Bonaire, Sint Eustatius and Saba", "Bosnia and Herzegovina", "Botswana",
+      "Brazil", "British Virgin Islands", "Brunei", "Bulgaria", "Burkina Faso",
+      "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde",
+      "Cayman Islands", "Central African Republic", "Chad", "Chile", "China",
+      "Christmas Island", "Cocos (Keeling) Islands", "Colombia", "Comoros",
+      "Congo", "Cook Islands", "Costa Rica", "Croatia", "Cuba", "Cura\u00e7ao",
+      "Cyprus", "Czech Republic", "Democratic Republic of the Congo", "Denmark",
+      "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador",
+      "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia",
+      "Ethiopia", "Falkland Islands (Malvinas)", "Faroe Islands", "Fiji",
+      "Finland", "France", "French Guiana", "French Polynesia", "Gabon",
+      "Gambia", "Georgia", "Germany", "Ghana", "Gibraltar", "Greece",
+      "Greenland", "Grenada", "Guadeloupe", "Guam", "Guatemala", "Guernsey",
+      "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong",
+      "Hungary", "Iceland", "India", "Indonesia", "Iraq", "Ireland",
+      "Isle of Man", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan",
+      "Jersey", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait",
+      "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya",
+      "Liechtenstein", "Lithuania", "Luxembourg", "Macau", "Macedonia",
+      "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta",
+      "Marshall Islands", "Martinique", "Mauritania", "Mauritius", "Mayotte",
+      "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro",
+      "Montserrat", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru",
+      "Nepal", "Netherlands", "New Caledonia", "New Zealand", "Nicaragua",
+      "Niger", "Nigeria", "Niue", "Norfolk Island", "Northern Mariana Islands",
+      "Norway", "Oman", "Pakistan", "Palau", "Palestinian Territories",
+      "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines",
+      "Pitcairn Islands", "Poland", "Portugal", "Puerto Rico", "Qatar",
+      "R\u00e9union", "Romania", "Russia", "Rwanda", "Saint Barth\u00e9lemy",
+      "Saint Helena", "Saint Kitts and Nevis", "Saint Lucia", "Saint Martin",
+      "Saint Pierre and Miquelon", "Saint Vincent and the Grenadines", "Samoa",
+      "San Marino", "S\u00e3o Tom\u00e9 and Pr\u00edncipe", "Saudi Arabia",
+      "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore",
+      "Sint Maarten", "Slovakia", "Slovenia", "Solomon Islands", "Somalia",
+      "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka",
+      "Sudan", "Suriname", "Svalbard and Jan Mayen", "Swaziland", "Sweden",
+      "Switzerland", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo",
+      "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan",
+      "Turks and Caicos Islands", "Tuvalu", "U.S. Virgin Islands", "Uganda",
+      "Ukraine", "United Arab Emirates", "United Kingdom", "United States",
+      "Uruguay", "Uzbekistan", "Vanuatu", "Venezuela", "Vietnam",
+      "Wallis and Futuna", "Yemen", "Zambia", "Zimbabwe"
+      )
+
+
   ### Divide results into separate tables for processing #######################
 
   missing <-
@@ -206,12 +259,19 @@ helper_scrape_location_parse <- function(results) {
 
   one_length <-
     one_length %>%
-    mutate(city = "ONE NEED TO CHECK",
-           region = "ONE NEED TO CHECK",
-           country = "ONE NEED TO CHECK",
-           date = Sys.Date()) %>%
+    mutate(
+      city = NA_character_,
+      region = NA_character_,
+      country = map_chr(.data$raw, ~{
+        .x %>%
+          str_extract('(?<=">).*(?=</a>)') %>%
+          str_replace("<span>", "") %>%
+          str_replace("</span>", "")
+        }),
+      country = dplyr::if_else(.data$country %in% country_list, .data$country,
+                        "ONE NEED TO CHECK"),
+      date = Sys.Date()) %>%
     select(.data$property_ID, .data$city:.data$country, .data$raw, .data$date)
-
 
   high_length <-
     high_length %>%
