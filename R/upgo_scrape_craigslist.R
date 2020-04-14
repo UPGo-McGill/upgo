cf3ad34baaaa95dd2a6cbe6a522e004f3d3809a5
David Wachsmuth <david.wachsmuth@mcgill.ca>
2020-04-14 09:15:35 -0400

    Craigslist and Kijiji scrapers added

--- R/upgo_scrape_craigslist.R
+++ R/upgo_scrape_craigslist.R
@@ -1,32 +1,60 @@
-
+#' Function to scrape Craigslist listings
+#'
+#' \code{upgo_scrape_craigslist} scrapes all data from Craigslist rental
+#' listings in a given city.
+#'
+#' @param city A character scalar indicating the city to scrape. It must
+#' correspond to the domain prefix used for a city's Craigslist site (e.g.
+#' "sfbay" for the San Francisco Bay area).
+#' @param old_results A data frame. If the output of a previous run of
+#' \code{upgo_scrape_craigslist} is supplied, listings previously scraped will
+#' not be scraped again.
+#' @param timeout A positive numeric scalar. How long in seconds should the
+#' function pause between scrape attempts to avoid triggering cooldowns?
+#' @param quiet A logical vector. Should the function execute quietly, or should
+#' it return status updates throughout the function (default)?
+#' @return A table with one row per listing scraped.
+#' @importFrom dplyr %>%
+#' @importFrom purrr map_dfr
+#' @importFrom rvest html_node html_nodes html_text
+#' @importFrom stringr str_extract
+#' @importFrom xml2 read_html
+#' @export
 
 upgo_scrape_craigslist <- function(city, old_results = NULL, timeout = 0.2,
                                    quiet = FALSE) {
-  
+
+  ### Setup ####################################################################
+
+  .temp_url_list <- .temp_listings <- NULL
+
+
   ### Find number of listings ##################################################
-  
-  listings_page <- 
+
+  listings_page <-
     read_html(paste0(
       "https://",
-      city, 
+      city,
       ".craigslist.org/d/apts-housing-for-rent/search/apa?lang=en&cc=us"))
-  
-  listings_to_scrape <- 
-    listings_page %>% 
-    html_node(".totalcount") %>% 
+
+  listings_to_scrape <-
+    listings_page %>%
+    html_node(".totalcount") %>%
     html_text()
-  
+
   pages <- ceiling(as.numeric(listings_to_scrape) / 120)
-  
-  
+
+
   ### Scrape listing URLs ######################################################
-  
+
   # Initialize url list
   url_list <- list()
-  
+
+  on.exit(.temp_url_list <<- url_list)
+
   for (i in seq_len(pages)) {
-    
-    listings <- 
+
+    listings <-
       read_html(paste0(
         "https://",
         city,
@@ -34,146 +62,148 @@ upgo_scrape_craigslist <- function(city, old_results = NULL, timeout = 0.2,
         120 * (i - 1),
         "&lang=en&cc=us"
       ))
-    
-    url_list[[i]] <- 
+
+    url_list[[i]] <-
       suppressWarnings(
-        listings %>% 
-          html_nodes(".result-row") %>% 
+        listings %>%
+          html_nodes(".result-row") %>%
           str_extract('(?<=href=").*(?=" c)')
       )
-    
+
     if (!quiet) message("Page ", i, " of ", pages, " scraped.")
-    
+
   }
-  
+
   url_list <- unique(unlist(url_list))
-  
-  
+
+
   ### Remove duplicate listings if old_results is provided
-  
+
   if (!missing(old_results)) {
-    
+
     url_list <-
-      url_list[!str_replace(url_list, 
+      url_list[!str_replace(url_list,
                             "\\?lang=en&amp;cc=us", "") %in% old_results$url]
-    
+
   }
-  
-  .temp_urls <<- url_list 
-  
+
+
   ### Scrape individual pages ##################################################
-  
+
   listings <- list()
-  
+
+  on.exit(.temp_listings <<- listings, add = TRUE)
+
   for (i in seq_along(url_list)) {
-    
-    listings[[i]] <- 
+
+    listings[[i]] <-
       tryCatch({
-        url_list[[i]] %>% 
+        url_list[[i]] %>%
           read_html(options = "HUGE")
       }, error = function(e) NULL)
-    
+
     if (!quiet) message("Listing ", i, " of ", length(url_list), " scraped.")
-    
-    .temp_listings <<- listings
+
     Sys.sleep(timeout)
-    
+
   }
-  
-  
+
+
   ### Clean up #################################################################
-  
+
   listings <- listings[!map_lgl(listings, is.null)]
-  
-  
+
+
   ### Parse HTML ###############################################################
-  
+
   results <-
-    listings %>% 
+    listings %>%
     map_dfr(~{
       tibble(
         id = tryCatch({
-          .x %>% 
-            html_node(xpath = '/html/body/section[@class="page-container"]') %>% 
-            html_node(xpath = 'section[@class="body"]') %>% 
-            html_node(xpath = 'section[@class="userbody"]') %>% 
-            html_node(xpath = 'div[@class="postinginfos"]') %>% 
-            html_node("p") %>% 
-            html_text() %>% 
+          .x %>%
+            html_node(xpath = '/html/body/section[@class="page-container"]') %>%
+            html_node(xpath = 'section[@class="body"]') %>%
+            html_node(xpath = 'section[@class="userbody"]') %>%
+            html_node(xpath = 'div[@class="postinginfos"]') %>%
+            html_node("p") %>%
+            html_text() %>%
             str_extract('(?<=id: ).*')
         }, error = function(e) NA_character_),
         url =
-          .x %>% 
-          html_node(xpath = '/html/head/link[@rel="canonical"]/@href') %>% 
+          .x %>%
+          html_node(xpath = '/html/head/link[@rel="canonical"]/@href') %>%
           html_text(),
-        title = 
-          .x %>% 
-          html_node(xpath = '/html/head/title') %>% 
+        title =
+          .x %>%
+          html_node(xpath = '/html/head/title') %>%
           html_text(),
-        date = 
-          .x %>% 
-          html_node(xpath = '//*[@id="display-date"]/time/@datetime') %>% 
+        date =
+          .x %>%
+          html_node(xpath = '//*[@id="display-date"]/time/@datetime') %>%
           html_text(),
         price =
           tryCatch({
-            .x %>% 
-            html_node(xpath = '/html/body') %>% 
-              html_node(xpath = 'section[@class = "page-container"]') %>% 
-              html_node(xpath = 'section[@class = "body"]') %>% 
-              html_node(xpath = 'h2[@class = "postingtitle"]') %>% 
-              html_node(xpath = 'span/span[@class = "price"]') %>% 
-              html_text() %>% 
+            .x %>%
+            html_node(xpath = '/html/body') %>%
+              html_node(xpath = 'section[@class = "page-container"]') %>%
+              html_node(xpath = 'section[@class = "body"]') %>%
+              html_node(xpath = 'h2[@class = "postingtitle"]') %>%
+              html_node(xpath = 'span/span[@class = "price"]') %>%
+              html_text() %>%
               readr::parse_number()
           }, error = function(e) NA_real_),
         location =
-          .x %>% 
-          html_node(xpath = '/html/head') %>% 
-          html_node(xpath = 'meta[@name = "geo.position"]/@content') %>% 
+          .x %>%
+          html_node(xpath = '/html/head') %>%
+          html_node(xpath = 'meta[@name = "geo.position"]/@content') %>%
           html_text(),
         details =
           tryCatch({
-            .x %>% 
-              html_node(xpath = '/html/body/section[@class="page-container"]') %>% 
-              html_node(xpath = 'section[@class="body"]') %>% 
-              html_node(xpath = 'section[@class="userbody"]') %>% 
-              html_node(xpath = 'div[@class = "mapAndAttrs"]') %>% 
-              html_nodes(xpath = 'p') %>% 
-              html_text() %>% 
-              str_replace_all("\n", "") %>% 
-              str_replace_all("  ", " ") %>% 
-              str_replace_all("  ", " ") %>% 
-              str_replace_all("  ", " ") %>% 
-              str_replace_all("  ", " ") %>% 
-              str_extract('(?<= ).*(?= )') %>% 
+            .x %>%
+              html_node(xpath = '/html/body/section[@class="page-container"]') %>%
+              html_node(xpath = 'section[@class="body"]') %>%
+              html_node(xpath = 'section[@class="userbody"]') %>%
+              html_node(xpath = 'div[@class = "mapAndAttrs"]') %>%
+              html_nodes(xpath = 'p') %>%
+              html_text() %>%
+              str_replace_all("\n", "") %>%
+              str_replace_all("  ", " ") %>%
+              str_replace_all("  ", " ") %>%
+              str_replace_all("  ", " ") %>%
+              str_replace_all("  ", " ") %>%
+              str_extract('(?<= ).*(?= )') %>%
               paste(collapse = "; ")
             }, error = function(e) NA_character_),
         text =
           tryCatch({
-            .x %>% 
+            .x %>%
               html_node(xpath = '/html/body/section[@class="page-container"]'
-                        ) %>% 
-              html_node(xpath = 'section[@class= "body"]') %>% 
-              html_node(xpath = 'section[@class= "userbody"]') %>% 
-              html_node(xpath = 'section[@id = "postingbody"]') %>% 
-              html_text() %>% 
+                        ) %>%
+              html_node(xpath = 'section[@class= "body"]') %>%
+              html_node(xpath = 'section[@class= "userbody"]') %>%
+              html_node(xpath = 'section[@id = "postingbody"]') %>%
+              html_text() %>%
               str_replace(
                 "\n            QR Code Link to This Post\n            \n        \n", ""
-                ) %>% 
-              str_replace_all("•\t", "") %>% 
+                ) %>%
+              str_replace_all("\u2022\t", "") %>%
               str_replace_all("\n", " ")
           }, error = function(e) NA_character_),
-        photos = 
-          list(.x %>% 
-          html_nodes(xpath = '//*/img') %>% 
-          html_node(xpath = '@src') %>% 
-          html_text() %>% 
-          # `[`(2:length(.)) %>% 
-          str_replace("50x50c", "600x450") %>% 
+        photos =
+          list(.x %>%
+          html_nodes(xpath = '//*/img') %>%
+          html_node(xpath = '@src') %>%
+          html_text() %>%
+          # `[`(2:length(.)) %>%
+          str_replace("50x50c", "600x450") %>%
           unique())
         )
     })
-    
+
+  on.exit()
+
   results
-  
+
 }
 
--- R/upgo_scrape_kijiji.R
+++ R/upgo_scrape_kijiji.R
@@ -1,170 +1,476 @@
-upgo_scrape_kijiji <- function(urls, folder_name) {
+#' Function to scrape Kijiji listings
+#'
+#' \code{upgo_scrape_kijiji} scrapes all data from Kijiji rental listings in
+#' Montreal, Toronto or Vancouver.
+#'
+#' @param city A character scalar indicating the city to scrape. Currently
+#' accepts "montreal", "toronto" or "vancouver" as inputs.
+#' @param short_long A character scalar. Should short-term rentals ("short"),
+#' long-term rentals ("long") or both ("both", the default) be scraped?
+#' @param old_results A data frame. If the output of a previous run of
+#' \code{upgo_scrape_kijiji} is supplied, listings previously scraped will not
+#' be scraped again.
+#' @param timeout A positive numeric scalar. How long in seconds should the
+#' function pause between scrape attempts to avoid triggering cooldowns?
+#' @param quiet A logical vector. Should the function execute quietly, or should
+#' it return status updates throughout the function (default)?
+#' @return A table with one row per listing scraped.
+#' @importFrom dplyr %>%
+#' @importFrom purrr map_dfr
+#' @importFrom rvest html_node html_nodes html_text
+#' @importFrom stringr str_extract
+#' @importFrom xml2 read_html
+#' @export
 
-  remDr <- rD[["client"]]
-  remDr$setImplicitWaitTimeout(2000)
+upgo_scrape_kijiji <- function(city, short_long = "both", old_results = NULL,
+                               timeout = 0.2, quiet = FALSE) {
+
+  ### Setup ####################################################################
+
+  .temp_url_list_short <- .temp_url_list_long <- .temp_url_list <-
+    .temp_listings <- NULL
 
 
-  ## For loop to save full page contents #########################################
+  ### Construct listing page URL ###############################################
 
-  results <-
-    tibble(id = NA_real_,
-           url = NA_character_,
-           title = NA_character_,
-           date = as.Date(NA),
-           price = NA_real_,
-           location = NA_character_,
-           owner_name = NA_character_,
-           owner_link = NA_character_,
-           owner_listings = NA_integer_,
-           owner_age = NA_character_,
-           details = NA_character_,
-           text = NA_character_,
-           photos = list())
+  url_start <- "https://www.kijiji.ca/"
+  url_end <- "?ad=offering&siteLocale=en_CA"
 
-  on.exit(.temp_output <<- results)
+  if (city %in% c("montreal", "Montreal", "montr\u00e9al", "Montr\u00e9al")) {
 
-  for (i in seq_along(urls)) {
+    if (short_long %in% c("short", "both")) {
 
-    message(crayon::magenta(crayon::bold((glue::glue(
-      "Scraping listing {i} of {length(urls)}. {Sys.time()}"
-    )))))
+      city_short_1 <- "b-location-court-terme/ville-de-montreal/"
+      city_short_2 <- "c42l1700281"
 
-    # Go to page
-    remDr$navigate(paste0("http://www.kijiji.ca/", urls[[i]],
-                          "?siteLocale=en_CA"))
-
-    Sys.sleep(2)
-
-    # Fill in id in results table
-    results[i,]$id <- urls[[i]] %>% str_extract('(?<=/)[:digit:]*$')
-
-    # Fill in URL in results table
-    results[i,]$url <- urls[[i]]
-
-    # Exit early if listing has been removed
-    if (
-      str_detect(unlist(remDr$getCurrentUrl()), "adRemoved") |
-      suppressMessages(tryCatch({
-        expired <- remDr$findElement("id", "PageExpiredVIP"); TRUE
-      }, error = function(e) FALSE)) |
-      suppressMessages((tryCatch({
-        missing <- remDr$findElement(
-          "xpath", "//*[text()='Hmm... Apparently this page no longer exists.']");
-        TRUE
-      }, error = function(e) FALSE)))
-    ) {next}
-
-    # Retrieve title
-    title <- remDr$findElement("class", "title-2323565163")
-    title <- title$getElementAttribute("outerHTML")[[1]] %>%
-      str_extract('(?<=>).*(?=</h1>)')
-    results[i,]$title <- title
-
-    # Retrieve posting date
-    date <- remDr$findElement("class", "datePosted-383942873")
-    date <- date$getElementAttribute("outerHTML")[[1]] %>%
-      str_extract('(?<=datetime..).*(?=T)') %>%
-      as.Date()
-    results[i,]$date <- date
-
-    # Retrieve price
-    price <- remDr$findElement("class", "currentPrice-2842943473")
-    price <- price$getElementAttribute("outerHTML")[[1]] %>%
-      str_extract('(?<=content..).*(?=.>\\$)') %>%
-      as.numeric()
-    results[i,]$price <- price
-
-    # Retrieve location
-    location <- remDr$findElement("class", "address-3617944557")
-    location <- location$getElementAttribute("outerHTML")[[1]] %>%
-      str_extract('(?<=557..).*(?=</span)')
-    results[i,]$location <- location
-
-    # Retrieve owner name and link
-    if (suppressMessages(tryCatch({
-      owner <- remDr$findElement("class", "link-3552666815")
-      TRUE
-    }, error = function(e) FALSE))) {
-      owner <- owner$getElementAttribute("outerHTML")[[1]]
-      owner_name <- owner %>% str_extract('(?<=..>).*(?=</a>)')
-      owner_link <- owner %>% str_extract('(?<=href...).*(?=">)')
-      results[i,]$owner_name <- owner_name
-      results[i,]$owner_link <- owner_link
-
-      # Retrieve owner listings
-      owner_listings <- remDr$findElements("class", "line-2791721720")
-      owner_listings <-
-        (map_chr(owner_listings, ~{
-          .x$getElementAttribute("outerHTML")[[1]]
-        }))[[2]] %>%
-        str_extract('(?<=<span>).*(?=</span>)') %>%
-        as.integer()
-      results[i,]$owner_listings <- owner_listings
-
-      # Retrieve owner age
-      owner_age <- remDr$findElements("class", "text-910784403")
-      if (length(owner_age) > 0) {
-        owner_age <-
-          (map_chr(owner_age, ~{
-            .x$getElementAttribute("outerHTML")[[1]]
-          }))[[3]] %>%
-          str_extract('(?<=..>).*(?=</div>)')
-      } else owner_age <- NA_character_
-
-      results[i,]$owner_age <- owner_age
+      listings_url_short <-
+        paste0(url_start, city_short_1, city_short_2, url_end)
 
     }
 
-    # Retrieve details
-    details <- suppressMessages(
+    if (short_long %in% c("long", "both")) {
+
+      city_long_1 <- "b-apartments-condos/ville-de-montreal/"
+      city_long_2 <- "c37l1700281"
+
+      listings_url_long <-
+        paste0(url_start, city_long_1, city_long_2, url_end)
+
+    }
+
+  } else if (city %in% c("toronto", "Toronto")) {
+
+    if (short_long %in% c("short", "both")) {
+
+      city_short_1 <- "b-short-term-rental/city-of-toronto/"
+      city_short_2 <- "c42l1700273"
+
+      listings_url_short <-
+        paste0(url_start, city_short_1, city_short_2, url_end)
+
+    }
+
+    if (short_long %in% c("long", "both")) {
+
+      city_long_1 <- "b-apartments-condos/city-of-toronto/"
+      city_long_2 <- "c37l1700273"
+
+      listings_url_long <-
+        paste0(url_start, city_long_1, city_long_2, url_end)
+
+    }
+
+  } else if (city %in% c("vancouver", "Vancouver")) {
+
+    if (short_long %in% c("short", "both")) {
+
+      city_short_1 <- "b-short-term-rental/vancouver/"
+      city_short_2 <- "c42l1700287"
+
+      listings_url_short <-
+        paste0(url_start, city_short_1, city_short_2, url_end)
+
+    }
+
+    if (short_long %in% c("long", "both")) {
+
+      city_long_1 <- "b-apartments-condos/vancouver/"
+      city_long_2 <- "c37l1700287"
+
+      listings_url_long <-
+        paste0(url_start, city_long_1, city_long_2, url_end)
+
+    }
+  }
+
+  ### Get STR URLs #############################################################
+
+  if (short_long %in% c("short", "both")) {
+
+    on.exit(.temp_url_list_short <<- url_list_short)
+
+    listings_page <-
+      read_html(listings_url_short)
+
+    listings_to_scrape <-
+      listings_page %>%
+      html_node(xpath = '//*[@class="showing"]') %>%
+      html_text() %>%
+      str_extract('(?<= of ).*(?=( Ads)|( results))') %>%
+      parse_number() %>%
+      as.integer()
+
+    pages <- min(ceiling(listings_to_scrape / 40), 100)
+
+    url_list_short <- vector("list", pages)
+
+    for (i in seq_len(pages)) {
+
+      listings <-
+        read_html(paste0(
+          url_start,
+          city_short_1,
+          "page-",
+          i,
+          "/",
+          city_short_2,
+          url_end
+          ))
+
+      url_list_short[[i]] <-
+        suppressWarnings(
+          listings %>%
+          html_nodes(xpath = '//*[@class="title"]') %>%
+          str_extract('(?<=href=").*(?=" c)')
+        )
+
+      if (!quiet) message("\rSTR page ", i, " of ", pages, " scraped.",
+                          appendLF = FALSE)
+
+    }
+
+    url_list_short <- unique(unlist(url_list_short))
+
+    if (pages == 100) {
+
+      url_list_short_2 <- vector("list", pages)
+
+      for (i in seq_len(pages)) {
+
+        listings <-
+          read_html(paste0(
+            url_start,
+            city_short_1,
+            "page-",
+            i,
+            "/",
+            city_short_2,
+            url_end,
+            "&sort=dateAsc"
+          ))
+
+        url_list_short_2[[i]] <-
+          suppressWarnings(
+            listings %>%
+              html_nodes(xpath = '//*[@class="title"]') %>%
+              str_extract('(?<=href=").*(?=" c)')
+          )
+
+        if (!quiet) message("\r STR page ", i, " of ", pages,
+                            " scraped (in ascending order).",
+                            appendLF = FALSE)
+
+      }
+
+      url_list_short <- unique(c(url_list_short, unlist(url_list_short_2)))
+
+    }
+
+    message("\n", length(url_list_short), " STR listing URLs scraped.")
+  }
+
+
+  ### Get LTR URLs #############################################################
+
+  if (short_long %in% c("long", "both")) {
+
+    on.exit(.temp_url_list_long <<- url_list_long, add = TRUE)
+
+    listings_page <-
+      read_html(listings_url_long)
+
+    listings_to_scrape <-
+      listings_page %>%
+      html_node(xpath = '//*[@class="showing"]') %>%
+      html_text() %>%
+      str_extract('(?<= of ).*(?=( Ads)|( results))') %>%
+      parse_number() %>%
+      as.integer()
+
+    pages <- min(ceiling(listings_to_scrape / 40), 100L)
+
+    url_list_long <- vector("list", pages)
+
+    for (i in seq_len(pages)) {
+
       tryCatch({
-        remDr$findElement("class", "itemAttributeCards-2416600896")},
-        error = function(e) {
-          remDr$findElement("class", "attributeListWrapper-2108313769")
-        }))
-    details <- details$getElementAttribute("outerHTML")[[1]]
-    results[i,]$details <- details
 
-    # Retrieve text
-    text <- remDr$findElement("class", "descriptionContainer-3544745383")
-    text <- text$getElementAttribute("outerHTML")[[1]] %>%
-      str_replace_all("\\n", " ") %>%
-      str_extract('(?<=<p>).*(?=</p>)')
-    results[i,]$text <- text
+        listings <-
+          read_html(paste0(
+            url_start,
+            city_long_1,
+            "page-",
+            i,
+            "/",
+            city_long_2,
+            url_end
+          ))
 
-    # Retrieve photo URLs
-    photos <- remDr$findElements("class", "heroImageForPrint-1152389693")
-    photos <- photos %>% map_chr(~{
-      .x$getElementAttribute("outerHTML")[[1]] %>%
-        str_extract('(?<=src..).*(?=" class)')
-    })
+        url_list_long[[i]] <-
+          suppressWarnings(
+            listings %>%
+              html_nodes(xpath = '//*[@class="title"]') %>%
+              str_extract('(?<=href=").*(?=" c)')
+          )
 
-    results[i,]$photos <- list(photos)
+      }, error = function(e) url_list_long[[i]] <- NULL)
 
-    # Save photos
+      if (!quiet) message("\rLTR page ", i, " of ", pages, " scraped.",
+                          appendLF = FALSE)
 
-    dir.create(paste0(folder_name, "/", results[i,]$id))
-
-    if (length(photos) > 0) {
-      download.file(photos, paste0(
-        folder_name,
-        "/",
-        results[i,]$id,
-        "/image_",
-        seq_along(photos),
-        ".jpg"
-      ))
     }
 
-    # Save temporary output
+    url_list_long <- unique(unlist(url_list_long))
 
+    if (pages == 100) {
+
+      url_list_long_2 <- vector("list", pages)
+
+      for (i in seq_len(pages)) {
+        tryCatch({
+
+          listings <-
+            read_html(paste0(
+              url_start,
+              city_long_1,
+              "page-",
+              i,
+              "/",
+              city_long_2,
+              url_end,
+              "&sort=dateAsc"
+            ))
+
+          url_list_long_2[[i]] <-
+            suppressWarnings(
+              listings %>%
+                html_nodes(xpath = '//*[@class="title"]') %>%
+                str_extract('(?<=href=").*(?=" c)')
+            )
+
+        }, error = function(e) url_list_long_2[[i]] <- NULL)
+
+        if (!quiet) message("\rLTR page ", i, " of ", pages,
+                            " scraped (in ascending order).",
+                            appendLF = FALSE)
+
+      }
+
+      url_list_long <- unique(c(url_list_long, unlist(url_list_long_2)))
+
+    }
+
+    message("\n", length(url_list_long), " LTR listing URLs scraped.")
+  }
+
+
+  ### Combine URLs into single list ############################################
+
+  if (short_long == "both") {
+    url_list <- unique(c(url_list_short, url_list_long))
+  } else if (short_long == "short") {
+    url_list <- url_list_short
+  } else if (short_long == "long") {
+    url_list <- url_list_long
+  }
+
+  url_list <- url_list[!is.na(url_list)]
+
+  on.exit(.temp_url_list <<- url_list)
+
+  ## Remove duplicate listings if old_results is provided
+
+  if (!missing(old_results)) {
+
+    url_list <-
+      url_list[!str_replace(url_list, '^/', 'https://www.kijiji.ca/') %in%
+                 old_results$url]
 
   }
 
-  # Return output
+  message(length(url_list), " listings ready to be scraped.",
+          appendLF = TRUE)
+
+
+  ### Scrape individual pages ##################################################
+
+  listings <- vector("list", length(url_list))
+
+  on.exit(.temp_listings <<- listings, add = TRUE)
+
+  for (i in seq_along(url_list)) {
+
+    listings[[i]] <-
+      tryCatch({
+        paste0("https://www.kijiji.ca", url_list[[i]], "?siteLocale=en_CA") %>%
+          read_html(options = "HUGE")
+      }, error = function(e) NULL)
+
+    if (!quiet) message("\rListing ", i, " of ", length(url_list), " scraped.",
+                        appendLF = FALSE)
+
+    Sys.sleep(timeout)
+
+  }
+
+
+  ### Clean up #################################################################
+
+  listings <- listings[!map_lgl(listings, is.null)]
+
+  message("\n", length(listings), " successfully scraped.")
+
+
+  ### Parse HTML ###############################################################
+
+  results <-
+    listings %>%
+    map_dfr(~{
+
+      tryCatch({
+        parse_results_kijiji(.x)
+      }, error = function(e) {
+
+        url <-
+          .x %>%
+          html_node("head") %>%
+          html_node(xpath = 'link[@rel = "canonical"]/@href') %>%
+          html_text() %>%
+          paste0("?siteLocale=en_CA")
+
+        redownload <- read_html(url, options = "HUGE")
+
+        parse_results_kijiji(redownload)
+
+      })
+    })
 
   on.exit()
 
   results
 
 }
+
+
+#' Helper function to parse scraped Kijiji listings
+#'
+#' \code{parse_results_kijiji} parses a scraped Kijiji listing.
+#'
+#' @param .x A single scraped Kijiji listing, as retrieved with read_html.
+#' @return A one-row data frame.
+#' @importFrom dplyr %>% if_else tibble
+#' @importFrom purrr map_dfr
+#' @importFrom rvest html_node html_nodes html_text
+#' @importFrom readr parse_number
+#' @importFrom stringr str_detect
+
+parse_results_kijiji <- function(.x) {
+
+  expired_check <-
+    .x %>%
+    html_node(xpath = '//*[@id = "PageExpiredVIP"]') %>%
+    html_text()
+
+  if (!is.na(expired_check)) {
+    tibble(
+      id =
+        .x %>%
+        html_node("head") %>%
+        html_node(xpath = 'link[@rel = "canonical"]/@href') %>%
+        html_text() %>%
+        str_extract('(?<=/)[:digit:]*$'),
+      url =
+        .x %>%
+        html_node("head") %>%
+        html_node(xpath = 'link[@rel = "canonical"]/@href') %>%
+        html_text(),
+      title = NA_character_,
+      short_long = if_else(
+        str_detect(url, "v-location-court-terme|v-short-term-rental"),
+        "short",
+        "long"),
+      date = as.Date(NA),
+      price = NA_real_,
+      location = NA_character_,
+      details = NA_character_,
+      text = NA_character_,
+      photos = vector("list", 1)
+    )
+  } else {
+    tibble(
+      id =
+        .x %>%
+        html_node(xpath = '//*[@class = "adId-4111206830"]') %>%
+        html_text(),
+      url =
+        .x %>%
+        html_node("head") %>%
+        html_node(xpath = 'link[@rel = "canonical"]/@href') %>%
+        html_text(),
+      title =
+        .x %>%
+        html_node("head") %>%
+        html_node("title") %>%
+        html_text(),
+      short_long = if_else(
+        str_detect(url, "v-location-court-terme|v-short-term-rental"),
+        "short",
+        "long"),
+      date =
+        .x %>%
+        html_node(xpath = '//*/time/@datetime') %>%
+        html_text() %>%
+        as.Date(),
+      price =
+        .x %>%
+        html_node(xpath = '//*[@class = "priceContainer-1419890179"]') %>%
+        html_node(xpath = 'span') %>%
+        html_node(xpath = 'span/@content') %>%
+        html_text() %>%
+        parse_number(),
+      location =
+        .x %>%
+        html_node(xpath = '//*[@class = "address-3617944557"]') %>%
+        html_text(),
+      details =
+        .x %>%
+        html_node(xpath =
+                    '//*[@class = "attributeListWrapper-2108313769"]') %>%
+        html_text(),
+      text =
+        .x %>%
+        html_node(xpath =
+                    '//*[@class = "descriptionContainer-3544745383"]') %>%
+        html_node('div') %>%
+        html_text(),
+      photos = suppressWarnings(list(
+        .x %>%
+          html_nodes(
+            xpath =
+              '//*[@class = "heroImageBackground-4116888288 backgroundImage"]'
+          ) %>%
+          str_extract('(?<=image:url..).*(?=..;back)'))))
+  }
+
+}
+
