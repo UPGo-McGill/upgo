d86c35baee290fd0c05ba4bc0ed22bcb8a8ff6b0
David Wachsmuth <david.wachsmuth@mcgill.ca>
2020-05-28 21:44:32 -0400

    Added proxy and parallel processing support to upgo_download

--- R/upgo_download.R
+++ R/upgo_download.R
@@ -4,13 +4,29 @@
 #' enable progress reporting when used outside the main UPGo scraping functions.
 #'
 #' @param urls A character vector of URLs to download.
+#' @param proxies Character vector of IPs to use for proxy connections. If
+#' supplied, this must be at least as long as the number of cores.
 #' @param quiet A logical vector. Should the function execute quietly, or should
 #' it return status updates throughout the function (default)?
 #' @return A list of HTML objects.
 #' @export
 
 
-upgo_download <- function(urls, quiet = FALSE) {
+upgo_download <- function(urls, proxies = NULL, quiet = FALSE) {
+
+  # Prepare for parallel processing
+  if(requireNamespace("doFuture", quietly = TRUE)) {
+    doFuture::registerDoFuture()
+  }
+
+  if (!missing(proxies)) {
+
+    # Put proxy list in .upgo_env so it can be accessed from child functions
+    .upgo_env$proxy_list <- proxies
+
+    on.exit(rlang::env_unbind(.upgo_env, "proxy_list"), add = TRUE)
+
+  }
 
   results <- vector("list", length(urls))
 
